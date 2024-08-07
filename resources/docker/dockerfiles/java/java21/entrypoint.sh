72bab53681a6ccdcd3dde29785518116a15f9f39
bigcat3997 <vqt.develop@gmail.com>
2024-08-07 11:15:26 +0700

    add new java21 dockerfiles set up

---
+++ resources/docker/dockerfiles/java/java21/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Purpose: Run the Java application with provided options.
+# Reference link: https://docs.oracle.com/javase/tutorial/deployment/jar/run.html
+# --------------------------------------
+
+java -jar app.jar $OPTS
