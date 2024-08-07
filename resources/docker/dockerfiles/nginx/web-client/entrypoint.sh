ecde7061e4d9889c05a28aa9f10460cec9e058be
bigcat3997 <vqt.develop@gmail.com>
2024-08-07 11:15:44 +0700

    add new web-client nginx dockerfiles set up

---
+++ resources/docker/dockerfiles/nginx/web-client/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# Purpose: Start the Nginx server in the foreground.
+# Reference link: https://nginx.org/en/docs/beginners_guide.html
+# --------------------------------------
+
+nginx -g "daemon off;"
